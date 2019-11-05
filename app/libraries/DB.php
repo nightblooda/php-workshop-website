0e92b420af7cb6528c373016be2d3f11d223bb4b
nightblooda <himanshux7x7x7@gmail.com>
2019-11-05 23:43:55 +0530

    Finishing with DB.php class

--- app/libraries/DB.php
+++ app/libraries/DB.php
@@ -1,18 +1,10 @@
 <?php
 
-    /*
-    * PDO Database Class
-    * Connect to Database
-    * Create prepared statement
-    * Bind Values
-    * Return rows and results
-    */
     class DB
     {
         private $host = DB_HOST;
         private $user = DB_USER;
         private $pass = DB_PASS;
-        // private $charset = DB_CHARSET;
         private $dbname = DB_NAME;
         private $dbh;
         private $stmt;
@@ -37,6 +29,56 @@
                 echo $this->error;
             }
         }
+
+        // Prepare statement query
+        public function query($sql)
+        {
+            $this->stmt = $this->dbh->prepare($sql);
+        }
+
+        // Bind values
+        public function bind($param, $value, $type = null)
+        {
+            if(is_null($type)){
+                switch(true){
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                        break;
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                        break;
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                        break;
+                    default:
+                        $type = PDO::PARAM_STR;
+                        break;
+                }
+            }
+            $this->stmt->bindValue($param, $value, $type);
+       }
+
+       // Execute the prepared statement
+       public function execute()
+       {
+           return $this->stmt->execute();
+       }
+
+       // Get result set as array of objects
+       public function resultSet(){
+           $this->execute();
+           return $this->stmt->fetchAll();
+       }
+
+       // Get single record as object
+       public function single(){
+           $this->execute();
+           return $this->stmt->fetch(PDO::FETCH_ASSOC);
+       }
+       // Get result count
+       public function rowCount(){
+           return $this->stmt->rowCount();
+       }
 
     }
 ?>
