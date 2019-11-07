70b89c54128d906d9849e87f4c551b12b273445a
nightblooda <himanshux7x7x7@gmail.com>
2019-11-07 23:50:45 +0530

    Adding var.php to config

---
+++ app/config/var.php
@@ -0,0 +1,14 @@
+<?php
+
+  define('DB_HOST','localhost');
+  define('DB_USER','dbuser');
+  define('DB_PASS','dbpass');
+  define('DB_NAME','dbname');
+  define('DB_CHARSET','utf8');
+  define('APP_ROOT', dirname(dirname(__FILE__)));
+  define('URL_ROOT', 'http://website.com');
+  define('SITE_NAME', 'website.com');
+  define('APP_DATE', 'NOV 7, 2019');
+  define('APP_DATE_TIME_FORMAT', 'd/m/Y H:i:s');
+
+?>
